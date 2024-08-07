08ada73e8fffd25dc5341951856327b27c3a54ec
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-08-07 16:06:35 +0300

    edit prompt functionality added

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <section className="w-full max-w-full flex-start flex-col">
+    <section className="w-full max-w-full flex-start flex-col mb-4">
       <h1 className="head_text text-left">
         <span className="blue_gradient">{type} Post</span>
       </h1>
       <p className="desc text-left max-w-md">
-        {type} and share amazing prompts with the world, and let your
-        imagination run wild with any AI-powered platforms.
+        {type === "Create"
+          ? `Create and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platforms.`
+          : "Edit your prompt."}
       </p>
 
       <form
