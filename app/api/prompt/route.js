2bdc8bbe042559e8c50077c9269a36fe17e76f92
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-08-08 12:31:35 +0300

    revalidation fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,6 +1,7 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
+export const revalidate = 0; //revalidate api every 0 second
 export const GET = async (req) => {
   try {
     await connectToDB();
