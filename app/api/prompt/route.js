94aa940f7a2e04f471c3d4ee01a1d5f35867ea4a
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-08-08 12:25:59 +0300

    dynamic route fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,15 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
-export const GET = async (req) => {
+export const dynamic = "force-dynamic";
+export const GET = async (request) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
 
     return new Response(JSON.stringify(prompts), { status: 200 });
-  } catch (err) {
-    return new Response("Failed to fetch all prompts.", { status: 500 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
