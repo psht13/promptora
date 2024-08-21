bd0b06a18fd118ecea1d5fb9a973e4e24b69915c
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-08-21 14:10:52 +0300

    update: grammar checks

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,8 @@ const Home = () => {
         <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
-        Promptora is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts.
+        Promptora is an open-source AI prompting tool for the modern world to
+        discover, create, and share prompts.
       </p>
 
       <Feed />
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -88,7 +88,7 @@ const Feed = () => {
       <form className="relative w-full flex-center">
         <input
           type="text"
-          placeholder="Search for a tag or a username"
+          placeholder="Search for a text, tag, username, or email"
           value={searchText}
           onChange={handleSearchChange}
           required
